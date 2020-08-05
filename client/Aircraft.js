f3039720c97636cd7e0f66d43573e2d3045fe6ec
Erik Quinn <erikquinn2@gmail.com>
2020-08-05 15:06:07 -0400

    Look up fixes from NavigationLibrary and add to Aircraft._fixes

--- client/Aircraft.js
+++ client/Aircraft.js
@@ -1,3 +1,5 @@
+import NavigationLibrary from './navData/NavigationLibrary.js';
+
 export default class Aircraft {
     /**
      * @for Aircraft
@@ -8,6 +10,18 @@ export default class Aircraft {
         for (const key in aircraftData) {
             this[key] = aircraftData[key];
         }
+
+        this._fixes = [];
+
+        this._init();
+    }
+
+    _init() {
+        this._initFixes();
+    }
+
+    _initFixes() {
+        this._fixes = this._getFixesOnRoute();
     }
 
     /**
@@ -22,5 +36,46 @@ export default class Aircraft {
             `<td>${this.destination}</td><td>${this.altitude}</td><td>${this.groundSpeed}</td></tr>`;
 
         return html;
+    }
+
+    // TODO: ADD THE `FixModel` AND UPDATE THIS TO USE IT!
+    /**
+     * Return an array of fixes in the flightplan for which we know has a defined location in nav data
+     *
+     * @for Aircraft
+     * @method _getFixesOnRoute
+     * @returns {array} - array of fixes
+     * @private
+     */
+    _getFixesOnRoute() {
+        const elements = this._getRouteElements();
+        const fixList = [];
+
+        for (const element of elements) {
+            const position = NavigationLibrary.getPositionOfFix(element);
+
+            if (typeof position === 'undefined') {
+                continue;
+            }
+
+            fixList.push(position);
+        }
+
+        return fixList;
+    }
+
+    /**
+     * Return an array of all space-separated elements of a flightplan, regardless of their type/content
+     *
+     * @for Aircraft
+     * @method _getRouteElements
+     * @returns {array<string>} - array of strings; the strings will be fixes, airways, procedures, etc
+     * @private
+     */
+    _getRouteElements() {
+        const elements = this.fpRoute.split(' ');
+        const trimmedElements = elements.filter((element) => element !== '');
+
+        return trimmedElements;
     }
 }
