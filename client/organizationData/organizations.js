cbecc4d2fdb803a55bb6cc536dc2b07b48280e21
Erik Quinn <erikquinn2@gmail.com>
2021-02-12 18:26:43 -0500

    Add ZFW facility view

--- client/organizationData/organizations.js
+++ client/organizationData/organizations.js
@@ -1,9 +1,10 @@
 import { DCC } from './DCC.js';
 import { ZBW } from './ZBW.js';
 import { ZDC } from './ZDC.js';
+import { ZFW } from './ZFW.js';
 import { ZMA } from './ZMA.js';
 import { ZNY } from './ZNY.js';
 
 export const ORGANIZATIONS = {
-    DCC, ZBW, ZDC, ZMA, ZNY
+    DCC, ZBW, ZDC, ZFW, ZMA, ZNY
 };
