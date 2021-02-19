149fbf17e4b9939f7f509c01442e393126081b93
Erik Quinn <erikquinn2@gmail.com>
2021-02-19 18:17:16 -0500

    Add ZDV facility view

--- client/organizationData/organizations.js
+++ client/organizationData/organizations.js
@@ -1,10 +1,11 @@
 import { DCC } from './DCC.js';
 import { ZBW } from './ZBW.js';
 import { ZDC } from './ZDC.js';
+import { ZDV } from './ZDV.js';
 import { ZFW } from './ZFW.js';
 import { ZMA } from './ZMA.js';
 import { ZNY } from './ZNY.js';
 
 export const ORGANIZATIONS = {
-    DCC, ZBW, ZDC, ZFW, ZMA, ZNY
+    DCC, ZBW, ZDC, ZDV, ZFW, ZMA, ZNY
 };
