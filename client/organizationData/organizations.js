1a8c1abea83db7aabb0b3db90377366732899706
Erik Quinn <erikquinn2@gmail.com>
2021-02-26 21:07:04 -0500

    Add ZOA facility view

--- client/organizationData/organizations.js
+++ client/organizationData/organizations.js
@@ -5,7 +5,8 @@ import { ZDV } from './ZDV.js';
 import { ZFW } from './ZFW.js';
 import { ZMA } from './ZMA.js';
 import { ZNY } from './ZNY.js';
+import { ZOA } from './ZOA.js';
 
 export const ORGANIZATIONS = {
-    DCC, ZBW, ZDC, ZDV, ZFW, ZMA, ZNY
+    DCC, ZBW, ZDC, ZDV, ZFW, ZMA, ZNY, ZOA
 };
